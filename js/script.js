ea1326836259b40229a6fa0db2d950de896b29ea
Vlad <ermac1988@mail.ru>
2021-05-21 21:44:45 +0300

    Added audio duration and playlist

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 const MUSIC_PLAYER = document.querySelector('.music-player');
 const COVER = MUSIC_PLAYER.querySelector('.music-player__cover');
 const COVER_IMG = COVER.querySelector('img');
+const AUDIO_PLAYLIST = MUSIC_PLAYER.querySelector('.audio-playlist');
+const AUDIO_PLAYLIST_BUTTON = AUDIO_PLAYLIST.querySelector('.audio-playlist__button');
 const AUDIO = MUSIC_PLAYER.querySelector('.music-player__audio');
 const AUDIO_NAME = MUSIC_PLAYER.querySelector('.audio-info__name');
 const AUDIO_ARTIST = MUSIC_PLAYER.querySelector('.audio-info__artist');
 const BACKWARD_BTN = document.getElementById('backward');
 const PLAY_BTN = document.getElementById('play');
 const FORWARD_BTN = document.getElementById('forward');
-const AUDIO_TRACK = MUSIC_PLAYER.querySelector('.audio-progress');
+const AUDIO_TRACK = MUSIC_PLAYER.querySelector('.audio-progress__track');
+const AUDIO_TRACK_DURATION = MUSIC_PLAYER.querySelector('.audio-progress__duration');
 const AUDIO_TRACK_PLAYED = AUDIO_TRACK.querySelector('span');
 const REQUEST_URL = '../data.json';
 
@@ -88,6 +91,13 @@ const renderData = data => {
     const AUDIO_CURRENT_TIME = e.target.currentTime;
     const AUDIO_PROGRESS = (AUDIO_CURRENT_TIME / AUDIO_DURATION) * 100;
     AUDIO_TRACK_PLAYED.style.width = `${AUDIO_PROGRESS}%`;
+
+    const CONVERTED_DURATION = formatTime(AUDIO_DURATION);
+    const CONVERTED_CURRENT_TIME = formatTime(AUDIO_CURRENT_TIME);
+    AUDIO_TRACK_DURATION.textContent = `${CONVERTED_CURRENT_TIME}/${CONVERTED_DURATION}`;
+    console.log('Duration: ',CONVERTED_DURATION);
+    console.log('Current time: ', CONVERTED_CURRENT_TIME);
+
   });
 
   AUDIO_TRACK.addEventListener('click', (e) => {
@@ -95,7 +105,17 @@ const renderData = data => {
     const CLICK_X = e.offsetX;
     const AUDIO_DURATION = AUDIO.duration;
     AUDIO.currentTime = (CLICK_X / AUDIO_TRACK_WIDTH) * AUDIO_DURATION;
-  })
+
+    const IS_PLAYING = MUSIC_PLAYER.classList.contains('music-player--play');
+
+    if (!IS_PLAYING) {
+      playAudio();
+    }
+  });
+
+  AUDIO_PLAYLIST_BUTTON.addEventListener('click', (e) => {
+    AUDIO_PLAYLIST.classList.toggle('audio-playlist--active');
+  });
 }
 
 function getAudio(data) {
@@ -119,4 +139,13 @@ function pauseAudio() {
   PLAY_BTN.querySelector('i.fas').classList.remove('fa-pause');
   
   AUDIO.pause();
+}
+
+function formatTime(time) {
+  let minutes = 0|(time / 60);
+  let seconds = 0|(time % 60);
+
+  if (minutes < 10) { minutes = '0'+ minutes; }
+  if (seconds < 10) { seconds = '0' + seconds; }
+  return `${minutes}:${seconds}`;
 }
